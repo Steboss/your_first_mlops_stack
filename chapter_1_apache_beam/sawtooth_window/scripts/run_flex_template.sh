9a44fb0db1e62a2de08d6edab6975427259e04f6
sb-synth <stefano.bosisio@synthesia.io>
2024-05-27 19:20:13 +0100

    fix typo

--- chapter_1_apache_beam/sawtooth_window/scripts/run_flex_template.sh
+++ chapter_1_apache_beam/sawtooth_window/scripts/run_flex_template.sh
@@ -5,7 +5,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 export RUNNING_REGION="europe-west1"
 export FLEX_BUCKET="flex_templates_my_pipeline"
 export TEMPLATE_FILE="sawtooth_template.json"
-export DATAFLOW_GCS_LOCATION="gs://${FLEX_BUCKET}/${TEMPLATE_FILE}"
+export DATAFLOW_GCS_LOCATION="gs://${FLEX_BUCKET}/${TEMPLATE_FILE}.json"
 export INPUT_SUBSCRIPTION="projects/${PROJECT_ID}/subscriptions/input-sawtooth-window-sub"
 export OUTPUT_TOPIC="projects/${PROJECT_ID}/topics/example-output-sawtooth"
 
@@ -13,7 +13,7 @@ export OUTPUT_TOPIC="projects/${PROJECT_ID}/topics/example-output-sawtooth"
 echo "Running Flex Template"
 gcloud dataflow flex-template run ${PIPELINE_NAME} \
 --project=${PROJECT_ID} \
---template-file-gcs-location=${DATAFLOW_GCS_LOCATION} \
+--template-file-gcs-location="${DATAFLOW_GCS_LOCATION}.json" \
 --worker-region=${RUNNING_REGION} \
 --region=${RUNNING_REGION} \
 --worker-machine-type=n1-standard-2 \
