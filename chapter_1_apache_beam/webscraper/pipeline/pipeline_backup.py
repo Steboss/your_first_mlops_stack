da27d0a9dd70a6c37fc96e9c61cd778c55de3e2d
sb-synth <stefano.bosisio@synthesia.io>
2024-05-27 13:27:47 +0100

    modify setup

--- chapter_1_apache_beam/webscraper/pipeline/pipeline_backup.py
+++ chapter_1_apache_beam/webscraper/pipeline/pipeline_backup.py
@@ -1,5 +1,6 @@
 import argparse
 import apache_beam as beam
+from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io.gcp import gcsio
 import requests
@@ -7,14 +8,20 @@ from bs4 import BeautifulSoup
 from structlog import get_logger
 from io import BytesIO
 import matplotlib.pyplot as plt
+from google.cloud import storage
+from io import StringIO
+
 
 logger = get_logger()
 
 
 class ScrapeNews(beam.DoFn):
+    def __init__(self, current_element):
+        """ Constructor to receive the current element to process"""
+        self.current_element = current_element
+
     def process(self, element):
-        """ Transform to perform scraping
-        The input element here is a PCollection<List>"""
+        """ Transform to perform scraping"""
         def remove_duplicates_and_corresponding_elements(list1, list2):
             """ Remove duplicates from list1 and corresponding elements from list2"""
             # Find duplicates and their first occurrences
@@ -35,47 +42,46 @@ class ScrapeNews(beam.DoFn):
             return new_list1, new_list2
 
         base_url = "https://news.google.com/search"
+        query = f"{self.current_element}"
+        params = {'q': query, 'hl': 'en-US', 'gl': 'US', 'ceid': 'US:en'}
+        response = requests.get(base_url, params=params)
 
-        for input_element in element:
-            params = {'q': input_element, 'hl': 'en-US', 'gl': 'US', 'ceid': 'US:en'}
-            response = requests.get(base_url, params=params)
+        newspaper_names = []
+        newspaper_titles = []
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # newspaper titles
+            newspaper_titles_div = soup.find_all('div', class_='vr1PYe')
+            for news_title in newspaper_titles_div:
+                newspaper_names.append(news_title.text)
 
-            newspaper_names = []
-            newspaper_titles = []
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-                # newspaper titles
-                newspaper_titles_div = soup.find_all('div', class_='vr1PYe')
-                for news_title in newspaper_titles_div:
-                    newspaper_names.append(news_title.text)
+            # title
+            buttons = soup.find_all('button', {'aria-label': True})
+            for button in buttons:
+                if 'aria-label' in button.attrs:
+                    # remove "More -" from the title
+                    title = button.attrs['aria-label'].replace("More - ", "")
+                    newspaper_titles.append(title)
 
-                # title
-                buttons = soup.find_all('button', {'aria-label': True})
-                for button in buttons:
-                    if 'aria-label' in button.attrs:
-                        # remove "More -" from the title
-                        title = button.attrs['aria-label'].replace("More - ", "")
-                        newspaper_titles.append(title)
+        # remove the first 6 elements from titles
+        newspaper_titles = newspaper_titles[6:]
+        # as well as the last 2 elements
+        newspaper_titles = newspaper_titles[:-2]
 
-            # remove the first 6 elements from titles
-            newspaper_titles = newspaper_titles[6:]
-            # as well as the last 2 elements
-            newspaper_titles = newspaper_titles[:-2]
-
-            news_titles, news_names = remove_duplicates_and_corresponding_elements(newspaper_titles, newspaper_names)
-            # NB how we're returning the elemnts
-            yield element, (news_titles, news_names)
+        news_titles, news_names = remove_duplicates_and_corresponding_elements(newspaper_titles, newspaper_names)
+        # NB how we're returning the elemnts
+        yield news_titles, news_names
 
 
 class WordCloud(beam.DoFn):
-    def __init__(self, output_path):
+    def __init__(self, output_path, filename):
         """ We need a constructor to set the output path"""
         self.output_path = output_path
+        self.filename = filename
 
     def process(self, element):
         """ Generate a word cloud from the titles"""
-        filename, data = element
-        text = ' '.join(data[0])
+        text = ' '.join(element)
         wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
         # Prepare to save the image to GCS
         output = BytesIO()
@@ -87,7 +93,7 @@ class WordCloud(beam.DoFn):
         # Reset stream position
         output.seek(0)
         # Write to Google Cloud Storage
-        gcs_path = f"{self.output_path}/{filename}.png"
+        gcs_path = f"{self.output_path}/{self.filename}.png"
         gcs = gcsio.GcsIO()
         gcs.write(gcs_path, output.getvalue())
 
@@ -115,17 +121,26 @@ def run_pipeline(argv=None):
         region=known_args.region
     )
 
-    with beam.Pipeline(options=pipeline_options) as p:
-        # Read lines from the input file directly into a PCollection
-        lines = (p | 'ReadFromFile' >> beam.io.ReadFromText(known_args.input_file)
-                   | 'Split lines' >> beam.Map(lambda x: x.split('\n')))
 
-        # Apply the ScrapeNews transform
-        scraped = lines | 'ScrapeNews' >> beam.ParDo(ScrapeNews())
-        # Generate and save word clouds
-        _ = scraped | 'GenerateWordCloud' >> beam.ParDo(WordCloud(output_path=known_args.output_bucket))
+    # create a pipeline object
+    pipeline = beam.Pipeline(options=pipeline_options)
+    # read the input file
+    storage_client = storage.Client()
+    bucket = known_args.input_file.split("/")[2]
+    file_name = known_args.input_file.split("/")[-1]
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.blob(file_name)
+    blob = blob.download_as_string()
+    blob = blob.decode('utf-8')
+    blob = StringIO(blob)
+    input_elements = blob.readlines()
 
-    result = p.run()
+    for input_element in input_elements:
+        current_line = input_element.strip()
+        (pipeline | f'Scrape {current_line}' >> beam.ParDo(ScrapeNews(current_element=current_line))
+         | f'Save {current_line} on GCS' >> beam.ParDo(WordCloud(output_path=known_args.output_bucket, filename=current_line)))
+    # do not forget
+    result = pipeline.run()
     result.wait_until_finish()
 
 
