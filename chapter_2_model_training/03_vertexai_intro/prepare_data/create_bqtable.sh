268c4696007cdf0a8743cb75ab3f715d3317f0f3
sb-synth <stefano.bosisio@synthesia.io>
2024-06-01 10:45:14 +0100

    create table

--- chapter_2_model_training/03_vertexai_intro/prepare_data/create_bqtable.sh
+++ chapter_2_model_training/03_vertexai_intro/prepare_data/create_bqtable.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# install
+pip3 install -r requirements.txt
+
 # run the code
-python create_fake_dataset.py
+python3 create_fake_dataset.py
 
 # copy the file to the bucket
 gsutil cp fake_dataset.csv gs://vertexai_inputfiles/fake_dataset.csv
 
+
+bq --location=EU mk -d \
+    --default_table_expiration 3600 \
+    --description "VertexAI dataset" \
+    learning_vertexai
 # create a table from the file
 bq load --source_format=CSV --autodetect learning_vertexai.fake_dataset_1  gs://vertexai_inputfiles/fake_dataset.csv
 
