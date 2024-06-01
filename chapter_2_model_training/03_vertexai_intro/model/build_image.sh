c79ee58402719972bbee4489a1ae44fbafb93795
sb-synth <stefano.bosisio@synthesia.io>
2024-06-01 11:12:01 +0100

    fix docker

--- chapter_2_model_training/03_vertexai_intro/model/build_image.sh
+++ chapter_2_model_training/03_vertexai_intro/model/build_image.sh
@@ -4,7 +4,7 @@ export ARTIFACT_REGISTRY_REPO="randomforest-vertexai"
 export PROJECT_ID=$(gcloud config get-value project)
 
 gcloud artifacts repositories create ${ARTIFACT_REGISTSRY_REPO} \
-    --repository-format=apt \
+    --repository-format=docker \
     --location=europe-west2 \
     --description="Repo for vertexAI models" \
     --async
@@ -15,5 +15,5 @@ docker build --no-cache -t randomforest_vertexai -f docker/Dockerfile .
 echo "Tag Docker image"
 docker tag randomforest_vertexai europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/randomforest_vertexai:latest
 echo "Push Docker image"
-gcloud auth configure-docker
+gcloud auth configure-docker europe-west2-docker.pkg.dev
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/randomforest_vertexai:latest
