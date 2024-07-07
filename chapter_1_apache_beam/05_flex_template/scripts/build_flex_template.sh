1d89d71070c8febc2d729a098efd64c69c3770d6
sb-synth <stefano.bosisio@synthesia.io>
2024-07-07 18:59:50 +0100

    modify script

--- chapter_1_apache_beam/05_flex_template/scripts/build_flex_template.sh
+++ chapter_1_apache_beam/05_flex_template/scripts/build_flex_template.sh
@@ -6,16 +6,16 @@ export PROJECT_ID=$(gcloud config get-value project)
 export FLEX_BUCKET="flex_templates_my_pipeline"
 export TEMPLATE_FILE="normal_window_template.json"
 
-echo "Build Docker image"
-docker build --no-cache -t window_pipeline -f docker/Dockerfile .
-echo "Tag Docker image"
-docker tag window_pipeline europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/window_pipeline:latest
-echo "Push Docker image"
-docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/window_pipeline:latest
+# echo "Build Docker image"
+# docker build --no-cache -t window_pipeline -f docker/Dockerfile .
+# echo "Tag Docker image"
+# docker tag window_pipeline europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/window_pipeline:latest
+# echo "Push Docker image"
+# docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/window_pipeline:latest
 
 echo "*************************"
 echo "Build Flex Template"
-gcloud dataflow flex-template build gs://${FLEX_BUCKET}/${TEMPLATE_FILE}.json \
+gcloud dataflow flex-template build gs://${FLEX_BUCKET}/${TEMPLATE_FILE} \
 --image-gcr-path europe-west2-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/window_pipeline:latest \
 --sdk-language "PYTHON" \
 --metadata-file metadata/metadata.json \
